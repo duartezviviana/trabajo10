217e60560c0c7207ddba4cb13b81ec8be6af6197
duartezviviana <140275104+duartezviviana@users.noreply.github.com>
2023-08-27 13:02:46 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,24 @@
+// ejercicio 1- creamos dos variables string
+
+var nombre="viviana";
+var apellido="duartez";
+
+// paso 2 - muestre nombre y apellido
+console.log(nombre+ "" +apellido);
+
+//ejercicio 2 - cremos tres variable numericas
+
+var numero1=5;
+var numero2=10;
+var suma = numero1+numero2;
+
+//muestre la suma
+
+console.log( "resultado de suma" + suma)
+//creamos una variable numerica mas
+var numero3=5
+//mostramos la suma de la variable numerica mas la variable suma
+console.log(suma+numero3)
+
+
+
